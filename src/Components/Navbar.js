0fee076309a4705a7caaf1f34eff660365133610
Sahil <sahil.saxena.58555@gmail.com>
2023-01-24 11:33:12 +0530

    sixth-commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div style ={{display: 'flex', background: 'red',alignContent: 'flex-start',fontSize: '20pt'}}>
         <Link to="/HackerNews" style={{paddingRight: '10px',paddingLeft: '10px', textDecoration: 'none'}}>Hacker News</Link>
-        <Link to="/search" style={{textDecoration: 'none'}}>News</Link>
+        <Link to="/search" style={{textDecoration: 'none'}}>new</Link>
     </div>
   )
 }
